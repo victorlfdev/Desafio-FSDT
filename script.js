caee933c67ce9af69f7436b7c98dadf91d7cc9d7
Victor Lima <victorlf2002@gmail.com>
2024-06-07 09:47:11 -0300

    Formatando

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function submitFormulario(){    
+function submitFormulario() {
 
     var jsonData = JSON.stringify({
         "names": [
-            document.getElementById("nome1").value ,
-            document.getElementById("nome2").value ,
-            document.getElementById("nome3").value ,
-            document.getElementById("nome4").value ,
-            document.getElementById("nome5").value 
+            document.getElementById("nome1").value,
+            document.getElementById("nome2").value,
+            document.getElementById("nome3").value,
+            document.getElementById("nome4").value,
+            document.getElementById("nome5").value
         ],
         "message": document.getElementById("historia").value
     });
-console.log(jsonData);
+    console.log(jsonData);
 
-        //const resposta = await 
-        
-        fetch("https://fsdt-contact-acf4ab9867a7.herokuapp.com/contact", {
+    fetch("https://fsdt-contact-acf4ab9867a7.herokuapp.com/contact", {
         method: "POST",
-        body:  jsonData,
+        body: jsonData,
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
